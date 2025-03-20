f03958863f5e9d190db7dd72421c8bc502b97c74
zxcdenjir <rush.wars1592@gmail.com>
2025-03-20 15:42:50 +0300

    Улучшенная табличка

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿int[] salary = new int[12];
 int sum = 0;
-double avg, pensiya = 0, deviations = 0;
 int max_salary = 0, min_salary = 5000;
 int max_month = 0, min_month = 0;
 
@@ -22,28 +21,30 @@ for (int i = 0; i < salary.Length; i++)
     }
 }
 
-pensiya = sum * 0.02;
-avg = sum / salary.Length;
+double pensiya = sum * 0.02;
+double avg = sum / salary.Length;
 Math.Round(avg, 2);
 Console.WriteLine($"Средняя зарплата: ${avg}\n");
 
 string[] monthes = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
-Console.WriteLine("| {0, -8} | {1, -9} | {2, -26} | {3, -30}", "Месяц", "Зарплата", "Отложение в пенсионный фонд", "Отклонение от средней зарплаты");
-Console.WriteLine("|----------|-----------|-----------------------------|---------------------------------");
+Console.WriteLine("┌──────────┬───────────┬─────────────────────────────┬─────────────────────────────────┐");
+Console.WriteLine("│ {0, -8} │ {1, -9} │ {2, -26} │ {3, -31} │", "Месяц", "Зарплата", "Отложение в пенсионный фонд", "Отклонение от средней зарплаты");
+Console.WriteLine("├──────────┼───────────┼─────────────────────────────┼─────────────────────────────────┤");
 
 for (int i = 0; i < salary.Length; i++)
 {
-    Console.Write("| {0, -8} | ${1, -8} | ${2, -26} | ", monthes[i], salary[i], Math.Round(salary[i] * 0.02));
+    Console.Write("│ {0, -8} │ ${1, -8} │ ${2, -26} │ ", monthes[i], salary[i], Math.Round(salary[i] * 0.02));
 
-    if ((salary[i] - avg) > 0)
-        Console.WriteLine("+${0, -30}", salary[i] - avg);
-    else if ((salary[i] - avg) < 0)
-        Console.WriteLine("-${0, -30}", avg - salary[i]);
-    else if ((salary[i] - avg) == 0)
-        Console.WriteLine("{0, -30}", "нет");
+    //if ((salary[i] - avg) > 0)
+    //    Console.WriteLine("+${0, -30}│", salary[i] - avg);
+    //else if ((salary[i] - avg) < 0)
+    //    Console.WriteLine("-${0, -30}│", avg - salary[i]);
+    //else if ((salary[i] - avg) == 0)
+        Console.WriteLine("{0, -31} │", "нет");
     pensiya += salary[i] * 0.02;
 }
+Console.WriteLine("└──────────┴───────────┴─────────────────────────────┴─────────────────────────────────┘");
 
 Console.WriteLine($"\n{monthes[max_month]} ${max_salary} - максимальная зарплата");
 Console.WriteLine($"{monthes[min_month]} ${min_salary} - минимальная зарплата");
